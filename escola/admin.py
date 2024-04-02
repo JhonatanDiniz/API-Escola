ade1af728016c2d9c2356e84c416898cfbd56ca1
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-04-02 08:08:32 -0300

    Criado admin aluno e curso

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('nome',)
+    search_fields = ('nome', 'cpf')
+admin.site.register(Aluno, AlunoAdmin)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('codigo_curso', 'descricao', 'nivel')
+    list_display_links = ('codigo_curso',)
+    search_fields =('codigo_curso', 'descricao',)
+admin.site.register(Curso,CursoAdmin)

b7efccea0019b868c0d8f87f8d9138929dcc26d4
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-04-03 21:21:58 -0300

    Incluido autenticacao

--- escola/views.py
+++ escola/views.py
@@ -1,27 +1,39 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaMatriculasCursoSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 
 class AlunoViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
 
 class ListaMatriculasAluno(generics.ListAPIView):
     def get_queryset(self):
         queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasAlunoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
 
 class ListaMatriculasCurso(generics.ListAPIView):
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasCursoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
